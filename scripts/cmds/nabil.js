a29584b0c7bb6e7c3a5b20875b71dc3d4c25afa2
kanazahid929 <puresingleking@gmail.com>
2026-05-26 01:59:14 +0600

    Update nabil.js

--- scripts/cmds/nabil.js
+++ scripts/cmds/nabil.js
@@ -4,28 +4,26 @@ const path = require("path");
 
 module.exports = {
 	config: {
-		name: "I love you ",
+		name: "iloveyou",
 		version: "1.0",
 		author: "siyam887754281",
 		countDown: 5,
 		role: 0,
 		shortDescription: "sarcasm",
-		longDescription: "Responds with random media when someone says ff",
+		longDescription: "Responds with random media when someone says I love you",
 		category: "reply",
 	},
 
 	onStart: async function () {},
 
 	onChat: async function ({ event, message }) {
-		if (event.body && event.body.toLowerCase() === "I love you") {
+		if (!event.body) return;
+
+		if (event.body.toLowerCase().trim() === "i love you") {
+
 			const mediaLinks = [
-				
 				"https://drive.google.com/uc?id=1nyrYWzMxoR3ivdoLqeY-rCexr38Bvlry"
-
-				
 			];
-
-	
 
 			const randomLink = mediaLinks[Math.floor(Math.random() * mediaLinks.length)];
 			const filePath = path.join(__dirname, "ff.mp4");
@@ -38,7 +36,11 @@ module.exports = {
 				fs.writeFileSync(filePath, response.data);
 
 				await message.reply({
-					body: "‎‎‎‎‎‎‎‎‎‎•───➺›𝐈 𝐋𝐨𝐯𝐞 𝐘𝐨𝐮 𝐓𝐨 𝐗ꫝ𝐧𝐒 ‎‎‎‎‎‎‎‎😘🩷🪶🪽🪄  \n\n ⎯͢⎯⃝💚🍒-༢Ꮗ𝐓𝐮𝐦𝐢𝐢 𝐀𝐦𝐚𝐤𝐞 𝐅𝐮𝐥𝐥 𝐃𝐢𝐨 𝐀𝐦𝐢𝐢\n𝐓𝐮𝐦𝐚𝐤𝐞 𝐅𝐮𝐥𝐞𝐫𝐫 𝐌𝐨𝐭𝐨 𝐕𝐚𝐥𝐨𝐛𝐚𝐬𝐛𝐨:)—♡<𝟯🌷🫶🥺🩷⏤͟͟͞͞◇💜✨⎯͢     \n\n‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎⎯͢⎯⃝🩷🪶🪽🪄 🥰",
+					body: `‎‎‎‎‎‎‎‎‎‎•───➺ː ›𝐈 𝐋𝐨𝐯𝐞 𝐘𝐨𝐮 𝐓𝐨 𝐗ꫝ𝐧'𝐒 <:)‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎😘🩷🪶🪽🪄      
+
+⎯͢⎯⃝💚🍒-༢"Ꮗ'𝐓𝐮𝐦𝐢𝐢 𝐀𝐦𝐚𝐤𝐞 𝐅𝐮𝐥𝐥 𝐃𝐢𝐨 𝐀𝐦𝐢𝐢 𝐓𝐮𝐦𝐚𝐤𝐞 𝐅𝐮𝐥𝐞𝐫𝐫 𝐌𝐨𝐭𝐨 𝐕𝐚𝐥𝐨𝐛𝐚𝐬𝐛𝐨:)—♡<"𝟯🌷🫶🥺🩷⏤͟͟͞͞◇💜✨⎯͢     
+
+‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎⎯͢⎯⃝🩷🪶🪽🪄 🥰`,
 					attachment: fs.createReadStream(filePath)
 				});
 
